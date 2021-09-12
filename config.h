93660746676444ea541f2748564b70e573ea65f6
vuckale <56733276+vuckale@users.noreply.github.com>
2021-09-12 16:28:29 +0200

    remove extra column and row

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 // USB Device descriptor parameter
-#define VENDOR_ID       0x049F
-#define PRODUCT_ID      0x000E
+#define VENDOR_ID       0x0210
+#define PRODUCT_ID      0x002B
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Ampex
 #define PRODUCT         210 Plus
@@ -14,15 +14,15 @@
 #define CORE_TEENSY
 
 /* key matrix size */
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 17
+#define MATRIX_ROWS 11
+#define MATRIX_COLS 16
 
 #define MATRIX_ROW_PINS \
-    { E6, E7, B0, B1, B2, B3, B4, B5, C7, F0, F2, F4}
+    { E6, E7, B0, B1, B2, B3, B4, B5, C7, F0, F2}
 #define MATRIX_COL_PINS \
-    { B7, D0, D1, D2, F6, F7, D7, E0, E1, C0, C3, C4, C5, C6, F1, F3, F5}
+    { B7, D0, D1, D2, F6, F7, D7, E0, E1, C0, C3, C4, C5, C6, F1, F3}
 #define UNUSED_PINS \
-    { D3, D5, D6, C1, C2, A0, A1, A2, A3, A4, A5, A6, A7, E4, E5}
+    { D3, D5, D6, C1, C2, A0, A1, A2, A3, A4, A5, A6, A7, E4, E5, F4, F5}
 
 #define DIODE_DIRECTION COL2ROW
 
@@ -35,34 +35,7 @@
 #define BOOTMAGIC_LITE_ROW 5
 #define BOOTMAGIC_LITE_COLUMN 2
 
-/* COL2ROW or ROW2COL */
-// #define DIODE_DIRECTION COL2ROW
-
 // Set 0 if debouncing isn't needed
 #define DEBOUNCE 5
-
-// Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
-// #define LOCKING_SUPPORT_ENABLE
-// Locking resynchronize hack
-#define LOCKING_RESYNC_ENABLE
-
-// Disable RGB Backlight
-#define RGB_BACKLIGHT_ENABLED 0
-
-// Define layers count
-#ifndef DYNAMIC_KEYMAP_LAYER_COUNT
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 12
-#endif
-
-// Define keymap macro count
-#ifndef DYNAMIC_KEYMAP_MACRO_COUNT
-#    define DYNAMIC_KEYMAP_MACRO_COUNT 16
-#endif
-
-// This is the default EEPROM max address to use for dynamic keymaps.
-// The default is the Teensy 2.0++ EEPROM max address.
-// Explicitly override it if the keyboard uses a microcontroller with
-// more or less EEPROM.
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
 
 #define MATRIX_HAS_GHOST
